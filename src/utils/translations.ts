985e8ee22335d631a167792e0098d53ae46fee65
Hillkim <hillkim@pm.me>
2023-12-04 16:18:58 +0300

    translate the app

---
+++ src/utils/translations.ts
@@ -0,0 +1,210 @@
+const translations = {
+  en: {
+    login: {
+      title: 'Welcome back!',
+      subtitle: 'Log in to your account',
+      forgotPassword: 'Forgot password?',
+      signIn: 'Sign in',
+      dontHaveAccount: "Don't have an account?",
+      signUp: 'Sign up',
+      or: 'Or',
+      signInWithGoogle: 'Sign in with Google',
+      email: 'Email',
+      password: 'Password',
+      invalidEmail: 'Enter a valid email',
+      invalidPassword: 'Enter a valid password'
+    },
+    signup: {
+      title: 'Sign up',
+      subTitle: 'Create an account to upload files',
+      welcome: 'Welcome to Delawvery!',
+      email: 'Email',
+      password: 'Password',
+      confirmPassword: 'Confirm Password',
+      alreadyHaveAccount: 'Already have an account?',
+      signIn: 'Sign in',
+      or: 'Or',
+      signUpWithGoogle: 'Sign up with Google',
+      invalidEmail: 'Enter a valid email',
+      invalidPassword: 'Enter a valid password'
+    },
+    table: {
+      title: 'All Uploaded Files',
+      subtitle: 'View all uploaded files',
+      searchPlaceholder: 'Search for a file',
+      uploadFile: 'Upload a new file',
+      noFiles: 'No files uploaded yet',
+      next: 'Next',
+      prev: 'Prev',
+      showing: 'Showing 1 to 4 of 50 Entries',
+      tableTitles: [
+        'File Name',
+        'File Type',
+        'Uploaded On',
+        'Number of Pages',
+        'Size of File',
+        'FILE Actions'
+      ],
+      actions: {
+        view: 'View',
+        delete: 'Delete'
+      }
+    },
+    upload: {
+      title: 'Upload a file',
+      description: 'Add a file to be uploaded',
+      attachDocument: 'Attach Document',
+      dragDropFiles:
+        'Drag and drop a file here or select a file from your computer',
+      fileType: 'File type: doc, docx, pdf or image',
+      upload: 'Upload',
+      addPages: 'Add number of pages to continue',
+      name: 'Name',
+      type: 'Type',
+      size: 'Size',
+      pages: 'Pages',
+      invalidFileType: 'File type not supported',
+      validNumberOfPages: 'Please enter a valid number of pages',
+      addSucess: 'File added successfully',
+      filePagesNotSupported:
+        'File type does not support automatic page counting, please enter the number of pages manually.',
+      numberOfPages: 'Number of pages',
+      files: {
+        pdf: 'PDF',
+        docx: 'DOCX',
+        doc: 'DOC',
+        jpeg: 'JPEG',
+        png: 'PNG',
+        jpg: 'JPG',
+        gif: 'GIF',
+        bmp: 'BMP',
+        tiff: 'TIFF'
+      }
+    },
+    footer: {
+      conectWithus: 'Connect with us on'
+    },
+    header: {
+      addFile: 'Add file',
+      files: 'Files',
+      logout: 'Logout'
+    },
+    messages: {
+      signInSuccess: 'Signed in successfully!',
+      signUpSuccess: 'Signed up successfully!',
+      signOutSuccess: 'Signed out successfully!',
+      signInFailed: "Couldn't sign in. Please try again.",
+      signUpFailed: "Couldn't sign up. Please try again.",
+      fileUploadSuccess: 'File uploaded successfully!',
+      fileUploadFailed: "Couldn't upload file. Please try again.",
+      fileDeleteSuccess: 'File deleted successfully!',
+      fileDeleteFailed: "Couldn't delete file. Please try again.",
+      languageChangedTo: 'Language changed to English'
+    }
+  },
+
+  he: {
+    login: {
+      title: 'ברוך שובך!',
+      subtitle: 'התחבר לחשבון שלך',
+      forgotPassword: 'שכחת סיסמא?',
+      signIn: 'התחבר',
+      dontHaveAccount: 'אין לך חשבון?',
+      signUp: 'הירשם',
+      or: 'או',
+      signInWithGoogle: 'התחבר עם גוגל',
+      email: 'אימייל',
+      password: 'סיסמא',
+      invalidEmail: 'אנא הכנס אימייל תקין',
+      invalidPassword: 'אנא הכנס סיסמא תקינה'
+    },
+    signup: {
+      title: 'הירשם',
+      subTitle: 'צור חשבון כדי להעלות קבצים',
+      welcome: 'ברוך הבא לדליוורי!',
+      email: 'אימייל',
+      password: 'סיסמא',
+      confirmPassword: 'אשר סיסמא',
+      alreadyHaveAccount: 'יש לך חשבון?',
+      signIn: 'התחבר',
+      or: 'או',
+      signUpWithGoogle: 'הירשם עם גוגל',
+      invalidEmail: 'אנא הכנס אימייל תקין',
+      invalidPassword: 'אנא הכנס סיסמא תקינה'
+    },
+    table: {
+      title: 'כל הקבצים שהועלו',
+      subtitle: 'צפה בכל הקבצים שהועלו',
+      searchPlaceholder: 'חפש קובץ',
+      uploadFile: 'העלה קובץ חדש',
+      noFiles: 'לא הועלו קבצים עדיין',
+      next: 'הבא',
+      prev: 'הקודם',
+      showing: 'מציג 1 עד 4 מתוך 50 רשומות',
+      tableTitles: [
+        'שם הקובץ',
+        'סוג הקובץ',
+        'הועלה בתאריך',
+        'מספר עמודים',
+        'גודל הקובץ',
+        'פעולות'
+      ],
+      actions: {
+        view: 'צפה',
+        delete: 'מחק'
+      }
+    },
+    upload: {
+      title: 'העלה קובץ',
+      description: 'העלה את הקובץ שלך',
+      attachDocument: 'צרף מסמך',
+      dragDropFiles: 'גרור ושחרר קובץ לכאן או בחר קובץ מהמחשב שלך',
+      fileType: 'סוג קובץ: doc, docx, pdf או תמונה',
+      upload: 'העלה',
+      addPages: 'הוסף מספר עמודים להמשך',
+      name: 'שם',
+      type: 'סוג',
+      size: 'גודל',
+      pages: 'עמודים',
+      invalidFileType: 'סוג קובץ לא נתמך',
+      validNumberOfPages: 'אנא הכנס מספר עמודים תקין',
+      addSuccess: 'הקובץ הועלה בהצלחה',
+      filePagesNotSupported:
+        'סוג הקובץ אינו תומך בספירת עמודים אוטומטית, אנא הכנס את מספר העמודים באופן ידני',
+      numberOfPages: 'מספר עמודים',
+      files: {
+        pdf: 'PDF',
+        docx: 'DOCX',
+        doc: 'DOC',
+        jpeg: 'JPEG',
+        png: 'PNG',
+        jpg: 'JPG',
+        gif: 'GIF',
+        bmp: 'BMP',
+        tiff: 'TIFF'
+      }
+    },
+    header: {
+      addFile: 'הוסף קובץ',
+      files: 'קבצים',
+      logout: 'התנתק'
+    },
+    footer: {
+      conectWithus: 'התחברו אלינו ב'
+    },
+    messages: {
+      signInSuccess: 'התחברת בהצלחה!',
+      signUpSuccess: 'נרשמת בהצלחה!',
+      signOutSuccess: 'התנתקת בהצלחה!',
+      signInFailed: 'לא ניתן להתחבר. אנא נסה שוב.',
+      signUpFailed: 'לא ניתן להירשם. אנא נסה שוב.',
+      fileUploadSuccess: 'הקובץ הועלה בהצלחה!',
+      fileUploadFailed: 'לא ניתן להעלות את הקובץ. אנא נסה שוב.',
+      fileDeleteSuccess: 'הקובץ נמחק בהצלחה!',
+      fileDeleteFailed: 'לא ניתן למחוק את הקובץ. אנא נסה שוב.',
+      languageChangedTo: 'שפה הוחלפה לעברית'
+    }
+  }
+}
+
+export default translations
