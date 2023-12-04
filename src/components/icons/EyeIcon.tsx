1e1718b69869eee33d802c67b160befc3f5e8e50
Hillkim <hillkim@pm.me>
2023-12-04 16:17:33 +0300

    add reusable icons

---
+++ src/components/icons/EyeIcon.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from 'react'
+const EyeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    {...props}
+  >
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+    <circle cx={12} cy={12} r={3} />
+  </svg>
+)
+export default EyeIcon
---
+++ src/components/icons/index.tsx
@@ -0,0 +1,8 @@
+export { default as EyeIcon } from './EyeIcon'
+export { default as DeleteIcon } from './DeleteIcon'
+export { default as SearchIcon } from './SearchIcon'
+// export { default as UploadIcon } from './UploadIcon'
+// export { default as GoogleIcon } from './GoogleIcon'
+// export { default as FacebookIcon } from './FacebookIcon'
+// export { default as TwitterIcon } from './TwitterIcon'
+// export { default as GithubIcon } from './GithubIcon'
